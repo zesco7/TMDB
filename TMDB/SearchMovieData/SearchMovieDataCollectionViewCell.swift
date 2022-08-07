27aaf3571cc733381b15a19d61dd78950371708b
hello <hello>
2022-08-08 08:57:43 +0900

    cellforat 수정

--- TMDB/SearchMovieData/SearchMovieDataCollectionViewCell.swift
+++ TMDB/SearchMovieData/SearchMovieDataCollectionViewCell.swift
@@ -23,64 +23,12 @@ class SearchMovieDataCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var divisionLIne: UIView!
     @IBOutlet weak var goToDetailPageButton: UIButton!
     
-    var list : [MovieModel] = []
+//    func displayImage() {
+//    self.previewImage.kf.setImage(with:URL(string: previewImageUrl)!)
+//    }
     
-    func dataRequest() {
-            
-        let apiKey = APIKey.TMDB
-        let url = EndPoint.searchMovieDataURL + "movie/day?api_key=\(apiKey)"
-        
-        AF.request(url, method: .get).validate(statusCode: 200..<400).responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-                print("JSON: \(json)")
-                
-                for i in 0...19 {
-                        
-                let releasedDate = json["results"][i]["release_date"].stringValue
-                print(releasedDate)
-                
-                //let genre = json["results"][i]["genre_ids"].stringValue
-                //print(genre)
-                
-                let previewImage = json["results"][i]["poster_path"].stringValue
-                print(previewImage)
-                
-                let previewImageUrl = "https://image.tmdb.org/t/p/w500/" + "\(previewImage)"
-                print(previewImageUrl)
-                
-                let title  = json["results"][i]["title"].stringValue
-                print(title)
-                
-                //let cast  = json["results"][0][""].stringValue
-                //print(cast)
-                    
-                let data = MovieModel(releasedDate: releasedDate, previewImage: previewImageUrl, title: title)
-                
-                self.list.append(data)
-                
-                self.releasedDate.text = releasedDate
-                //self.genre.text = genre
-                self.title.text = title
-                self.divisionLIne.layer.borderWidth = 1
-                self.divisionLIne.tintColor = .darkGray
-                self.goToDetailPageButton.setTitle("자세히 보기", for: .normal)
-                
-                self.previewImage.kf.setImage(with:URL(string: previewImageUrl)!)
-                
-                }
-                
-                
-                
-                
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-            
     
+            
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- TMDB/SearchMovieData/SearchMovieDataViewController.swift
+++ TMDB/SearchMovieData/SearchMovieDataViewController.swift
@@ -7,32 +7,86 @@
 
 import UIKit
 
+import Alamofire
+import Kingfisher
+import SwiftyJSON
 
 class SearchMovieDataViewController: UIViewController {
-
+    
     @IBOutlet weak var result: UICollectionView!
+    
+    var list : [MovieModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         result.delegate = self
         result.dataSource = self
         result.register(UINib(nibName: SearchMovieDataCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: SearchMovieDataCollectionViewCell.identifier)
-    
+        
+        dataRequest()
+        
     }
-
+    
+    func dataRequest() {
+        
+        let apiKey = APIKey.TMDB
+        let url = EndPoint.searchMovieDataURL + "movie/day?api_key=\(apiKey)"
+        
+        AF.request(url, method: .get).validate(statusCode: 200..<400).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+                for i in 0...19 {
+                    
+                    let openingDay = json["results"][i]["release_date"].stringValue
+                    print(openingDay)
+                    
+                    //let genre = json["results"][i]["genre_ids"].stringValue
+                    //print(genre)
+                    
+                    let previewImageUrl = "https://image.tmdb.org/t/p/w500/" + json["results"][i]["poster_path"].stringValue
+                    print(previewImageUrl)
+                    
+                    let movieTitle  = json["results"][i]["title"].stringValue
+                    print(movieTitle)
+                    
+                    //let cast  = json["results"][0][""].stringValue
+                    //print(cast)
+                    
+                    let data = MovieModel(releasedDate: openingDay, previewImage: previewImageUrl, title: movieTitle)
+                    
+                    self.list.append(data)
+                }
+                self.result.reloadData()
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
 extension SearchMovieDataViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return list.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchMovieDataCollectionViewCell.identifier, for: indexPath) as? SearchMovieDataCollectionViewCell else { return UICollectionViewCell() }
         
-        cell.dataRequest()
+        let data = list[indexPath.row]
+        print(data)
+        
+        cell.releasedDate.text = data.releasedDate
+        cell.title.text = data.title
+        //self.genre.text = genre
+        cell.divisionLIne.layer.borderWidth = 1
+        cell.divisionLIne.tintColor = .darkGray
+        cell.goToDetailPageButton.setTitle("자세히 보기", for: .normal)
         
         return cell
     }
